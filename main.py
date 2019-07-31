55046341096f09231a6420209bf4c0cdb5330453
asw91666 <53108353+asw91666@users.noreply.github.com>
2019-07-31 13:10:07 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ _, first_frame = cap.read()
 first_gray = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
 first_gray = cv2.GaussianBlur(first_gray, (5, 5), 0)
 
-# ..
+# frame을 하나하나 가져오는 while loop 를 돈다.
 while True:
     _, frame = cap.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -23,9 +23,10 @@ while True:
     cv2.imshow("Frame", frame)
     cv2.imshow("difference", difference)
 
-    key = cv2.waitKey(30)
+    key = cv2.waitKey(20)       #cv2.waitKey()는 ESC를 누르면 27을 RETURN
     if key == 27:
         break
+
 
 cap.release()
 cv2.destroyAllWindows()
@@ -70,6 +71,100 @@ while True:
 
     else:
         break
+
+
+'''
+
+'''
+
+
+
+import cv2
+import numpy as np
+
+img=cv2.imread('chun.png',cv2.IMREAD_COLOR)
+show_img=np.copy(img)
+
+mouse_pressed = False
+y = x = w = h = 0
+
+labels = np.zeros(img.shape[:2],np.uint8)
+
+#이미지에 네모박스 그리는 함수
+def mouse_callback(event, _x, _y, flags, param):
+    global show_img, x,y,w,h, mouse_pressed
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        mouse_pressed = True
+        x,y = _x, _y
+        show_img = np.copy(img)
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if mouse_pressed:
+            show_img = np.copy(img)
+            cv2.rectangle(show_img, (x,y),
+                          (_x, _y), (0, 255, 0), 3)
+
+        elif event == cv2.EVENT_LBUTTONUP:
+            mouse_pressed = False
+            w, h = _x, -x, _y - y   #이게 뭔지 모르겠넹
+
+
+label = cv2.GC_BGD
+lbl_clrs = {cv2.GC_BGD: (0, 0, 0), cv2.GC_FGD: (255, 255, 255)}
+
+def mouse_callback2(event, x, y, flags, param):
+    global mouse_pressed
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        mouse_pressed = True
+        cv2.circle(labels, (x, y), 5, label, cv2.FILLED)
+        cv2.circle(show_img, (x, y), 5, lbl_clrs[label],
+                   cv2.FILLED)
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if mouse_pressed:
+            cv2.circle(labels, (x, y), 5, label, cv2.FILLED)
+            cv2.circle(show_img, (x, y), 5, lbl_clrs[label],
+                       cv2.FILLED)
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        mouse_pressed = False
+
+cv2.namedWindow('image')
+cv2.setMouseCallback('image', mouse_callback2)
+
+while True:
+    cv2.imshow('image', show_img)
+    k = cv2.waitKey(1)          # k 는 뭐지 이건?
+
+    if k == ord('a') and not mouse_pressed:
+        break
+    elif k == ord('l'):
+        label = cv2.GC_FGD- label
+
+        
+
+cv2.destroyAllWindows()
+
+
+# 4.
+
+
+labels, bgdModel, fgdModel = cv2.grabCut(img, labels, (x,y,w,h), None, None, 5, cv2.GC_INIT_WITH_RECT)
+#labels, bgdModel, fgdModel = cv2.grabCut(img, labels, None, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_MASK)
+
+show_img = np.copy(img)
+show_img[(labels == cv2.GC_PR_BGD) |
+         (labels == cv2.GC_BGD)] //= 3
+
+cv2.imshow('image', show_img)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+# 5.
+
+
 
 
 '''
