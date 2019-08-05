046b34d387425b2cb636cb702716f68091b0b5a1
asw91666 <53108353+asw91666@users.noreply.github.com>
2019-08-05 16:55:26 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
 # Loading camera
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)           # 웹캠 번호가 0일수도, 1일수도 그 이상일 수도 있다.
 font = cv2.FONT_HERSHEY_PLAIN
 starting_time = time.time()
 frame_id = 0
@@ -23,7 +23,8 @@ while True:
     height, width, channels = frame.shape
 
  # Detecting objects
-    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    # 320*320 #416*416 #609*609 <=== 정확도 조절
+    blob = cv2.dnn.blobFromImage(frame, 0.00392, (320, 320), (0, 0, 0), True, crop=False)
     net.setInput(blob)
     outs = net.forward(output_layers)
     # Showing informations on the screen
@@ -35,19 +36,19 @@ while True:
             scores = detection[5:]
             class_id = np.argmax(scores)
             confidence = scores[class_id]
-            if confidence > 0.2:
+            if confidence > 0.1:
                 # Object detected
                 center_x = int(detection[0] * width)
                 center_y = int(detection[1] * height)
-                w = int(detection[2] * width)
+                w = int(detection[2] * width)   # width of object
                 h = int(detection[3] * height)
                 # Rectangle coordinates
-                x = int(center_x - w / 2)
+                x = int(center_x - w / 2)       #the starting X position of detected object
                 y = int(center_y - h / 2)
                 boxes.append([x, y, w, h])
-                confidences.append(float(confidence))
-                class_ids.append(class_id)
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.4, 0.3)
+                confidences.append(float(confidence))   #percentage
+                class_ids.append(class_id)  #the name of detected object
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.4, 0.3)    #NMS 알고리즘(한 물체를 두개의 물체로 인식하면 이거 조정하면 돼)
 
     for i in range(len(boxes)):
         if i in indexes:
