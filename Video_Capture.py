098ab5d6361f6610b9713b28ee555681ee1df2d8
asw91666 <53108353+asw91666@users.noreply.github.com>
2019-08-29 23:16:14 +0900

    Update Video_Capture.py

--- Video_Capture.py
+++ Video_Capture.py
@@ -1,7 +1,7 @@
 import cv2
 
 # 영상의 의미지를 연속적으로 캡쳐할 수 있게 하는 class
-vidcap = cv2.VideoCapture(1)    # 이 부분 바꿔서 동영상 파일을 가져와서 캡쳐하기가 가능.
+vidcap = cv2.VideoCapture('hand4.mp4')    # 이 부분 바꿔서 동영상 파일을 가져와서 캡쳐하기가 가능.
 
 count = 0
 frame_skip = 0
@@ -12,12 +12,16 @@ while (vidcap.isOpened()):
     # 두 함수를 동시에 불러오는 이유는 프레임이 존재하지 않을 때
     # grab() 함수를 이용하여 return false 혹은 NULL 값을 넘겨 주기 때문
     ret, image = vidcap.read()
-    frame_skip += 1
-    if(frame_skip % 20 == 0):
-        # 캡쳐된 이미지를 저장하는 함수
-        cv2.imwrite("images/frame%d.jpg" % count, image)
+    if ret:
+        frame_skip += 1
+        if(frame_skip % 10 == 0):
+            # 캡쳐된 이미지를 저장하는 함수
+            cv2.imwrite("hands_rawData/rframe%d.jpg" % count, image)
 
-        print('Saved frame%d.jpg' % count)
-        count += 1
+            print('Saved %d.jpg' % count)
+            count += 1
+    else:
+        break
+
 
 vidcap.release()
